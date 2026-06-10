123f2021b93bbb7aaf55e5bdee90175d7f19ef85
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2026-06-10 00:05:06 -0300

    Seccion 3-4.

---
+++ Seccion 4/primera-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <button @click="Saludar">Say Hello</button>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    Saludar(){
+      alert('Hello World')
+      this.title = 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: green;
+}
+</style>
